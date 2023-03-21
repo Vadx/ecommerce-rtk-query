53b2cbb70ce4950fe6b794b2d77777011522456d
Vadx <lv_km@ukr.net>
2023-03-21 17:37:51 +0200

    add theme

---
+++ theme/button.ts
@@ -0,0 +1,34 @@
+import type { ComponentStyleConfig } from "@chakra-ui/theme";
+
+export const Button: ComponentStyleConfig = {
+  baseStyle: {
+    borderRadius: "5px",
+    fontSize: "10pt",
+    fontWeight: 700,
+    _focus: {
+      boxShadow: "none",
+    },
+  },
+  sizes: {
+    sm: {
+      fontSize: "8pt",
+    },
+    md: {
+      fontSize: "10pt",
+    },
+  },
+  variants: {
+    solid: {
+      color: "white",
+      bg: "blue.500",
+      _hover: {
+        bg: "blue.400",
+      },
+    },
+    outline: {
+      color: "blue.500",
+      border: "1px solid",
+      borderColor: "blue.500",
+    },
+  },
+};
